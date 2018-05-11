cc9d2b5cdbf2fb0f5e32af70af686b5146e338ff
Walter Xie <walterxie@users.noreply.github.com>
2018-05-11 17:02:27 +1200

    add table

--- cban.js
+++ cban.js
@@ -1,33 +1,30 @@
 var xml = "packages2.5.xml"
-// Create a connection to the file.
-var Connect = new XMLHttpRequest();
-// Define which file to open and
-// send the request.
-Connect.open("GET", xml, false);
-Connect.setRequestHeader("Content-Type", "xml");
-Connect.send(null);
 
-if (Connect.readyState == 4 && Connect.status == 200) {
-	// Place the response in an XML document.
-	var TheDocument = Connect.responseXML;
-	// Place the root node in an element.
-	var Packages = TheDocument.childNodes[0];
-	// Retrieve each customer in turn.
-	for (var i = 0; i < Packages.children.length; i++) {
-		var Package = Packages.children[i];
-		// Access each of the data values.
-		var name = Package.getElementsByTagName("name");
-		var version = Package.getElementsByTagName("version");
-		var projectURL = Package.getElementsByTagName("projectURL");
+var conn = new XMLHttpRequest();
+conn.open("GET", xml, false);
+conn.onreadystatechange = function () {
+  if (conn.readyState == 4 && conn.status == 200) {
+    var doc = conn.responseXML;
+    var packages = doc.getElementsByTagName("package")
+    document.write('<table border="1">');
+    for (var pk in packages) {
+        var name = packages[pk].getAttribute("name");
+        var version = packages[pk].getAttribute("version");
+		var projectURL = packages[pk].getAttribute("projectURL");
 		// Write the data to the page.
 		document.write("<tr><td>");
-		document.write(name[0].textContent.toString());
+		document.write(name);
 		document.write("</td><td>");
-		document.write(version[0].textContent.toString());
+		document.write(version);
 		document.write("</td><td>");
-		document.write(projectURL[0].textContent.toString());
+		document.write(projectURL);
 		document.write("</td></tr>");
-	}
-} else if (xmlhttp.status==404) { 
+    }
+    document.write('</table>');
+    var num = packages.length;
+//     document.write('<p>num.toString().concat(" packages")</p>');
+  } else if (xmlhttp.status==404) { 
     alert(xml.concat(" could not be found")); 
-}
+  }
+};
+conn.send(null);
