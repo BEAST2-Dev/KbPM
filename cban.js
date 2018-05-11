2206ee0f709f3dbc0026dd6803e82b443cdd5479
Walter Xie <walterxie@users.noreply.github.com>
2018-05-11 15:11:30 +1200

    modify js

--- cban.js
+++ cban.js
@@ -2,28 +2,27 @@
   var Connect = new XMLHttpRequest();
   // Define which file to open and
   // send the request.
-  Connect.open("GET", "Customers.xml", false);
+  Connect.open("GET", "https://raw.githubusercontent.com/CompEvol/CBAN/blob/master/packages2.5.xml", false);
   Connect.setRequestHeader("Content-Type", "text/xml");
   Connect.send(null);
   // Place the response in an XML document.
   var TheDocument = Connect.responseXML;
   // Place the root node in an element.
-  var Customers = TheDocument.childNodes[0];
+  var Packages = TheDocument.childNodes[0];
   // Retrieve each customer in turn.
-  for (var i = 0; i < Customers.children.length; i++)
+  for (var i = 0; i < Packages.children.length; i++)
   {
-   var Customer = Customers.children[i];
+   var Package = Packages.children[i];
    // Access each of the data values.
-   var Name = Customer.getElementsByTagName("Name");
-   var Age = Customer.getElementsByTagName("Age");
-   var Color = Customer.getElementsByTagName(
-     "FavoriteColor");
+   var name = Package.getElementsByTagName("name");
+   var version = Package.getElementsByTagName("version");
+   var projectURL = Package.getElementsByTagName("projectURL");
    // Write the data to the page.
    document.write("<tr><td>");
-   document.write(Name[0].textContent.toString());
+   document.write(name[0].textContent.toString());
    document.write("</td><td>");
-   document.write(Age[0].textContent.toString());
+   document.write(version[0].textContent.toString());
    document.write("</td><td>");
-   document.write(Color[0].textContent.toString());
+   document.write(projectURL[0].textContent.toString());
    document.write("</td></tr>");
   }
