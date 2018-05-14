1a7cf66c35b2545c0e5f4fd501272b3c1f1f3906
Walter Xie <walterxie@users.noreply.github.com>
2018-05-14 14:34:23 +1200

    complete, but no unique function

--- cban.js
+++ cban.js
@@ -1,24 +1,53 @@
 var xml = "packages2.5.xml"
+var num_pkg = 0;
 
 var conn = new XMLHttpRequest();
 conn.open("GET", xml, false);
 conn.onreadystatechange = function () {
   if (conn.readyState == 4 && conn.status == 200) {
-    var doc = conn.responseXML;
-    var packages = doc.getElementsByTagName("package")
     document.write('<table border="1">');
+    //header
+    document.write("<tr><th>");
+    document.write("Package");
+    document.write("</th><th>");
+    document.write("Version");
+    document.write("</th><th>");
+    document.write("Description");
+    document.write("</th><th>");
+    document.write("Dependency");
+    document.write("</th></tr>");
+
+    var doc = conn.responseXML;
+    var packages = doc.getElementsByTagName("package");
     for (var pk in packages) {
         var name = packages[pk].getAttribute("name");
         var version = packages[pk].getAttribute("version");
 		var projectURL = packages[pk].getAttribute("projectURL");
+		var description = packages[pk].getAttribute("description");
 		// Write the data to the page.
 		document.write("<tr><td>");
-		document.write(name);
+		document.write("<a href=\"" + projectURL + "\">" + name + "</a>");
 		document.write("</td><td>");
 		document.write(version);
 		document.write("</td><td>");
-		document.write(projectURL);
+		document.write(description);
+		document.write("</td><td>");
+		// dependency
+		var dependency="", depends = packages[pk].getElementsByTagName("depends");
+		for(var i = 0; i < depends.length; i++) {
+		  if (i > 0) {
+			  dependency = dependency + ", ";
+		  }
+		  dependency = dependency + depends[i].getAttribute("on") + " (" + depends[i].getAttribute("atleast");
+		  if (depends[i].hasAttribute("atmost")) {       
+			  dependency = dependency + "-" + depends[i].getAttribute("atmost") + ")";
+          } else {
+			  dependency = dependency + "-?)";
+          }
+		}
+		document.write(dependency);
 		document.write("</td></tr>");
+		num_pkg ++;				
     }
     document.write('</table>');
     var num = packages.length;
@@ -28,3 +57,6 @@ conn.onreadystatechange = function () {
   }
 };
 conn.send(null);
+
+document.write("<p>Extract " + num_pkg + " packages from " + xml + "</p>");
+
